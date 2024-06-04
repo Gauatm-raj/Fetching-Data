c9b3f670026cbcfc62e768c6f23c6f8a42e51599
Gauatm-raj <gautamraj7569@gmail.com>
2024-06-04 15:25:51 +0530

    DONE

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ async function fetchData() {
       row.innerHTML = `
         <td><img src="${coin.image}" alt="">${coin.name}</td>
         <td>${coin.symbol}</td>
-        <td>${coin.current_price}</td>
-        <td>${coin.total_volume}</td>
+        <td>$ ${coin.current_price}</td>
+        <td>$ ${coin.total_volume}</td>
         <td>${coin.price_change_percentage_24h} %</td>
-        <td>${coin.market_cap}</td>
+        <td>$ ${coin.market_cap}</td>
       `;
       tableBody.appendChild(row);
     });
